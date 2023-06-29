fa00b119c437c2acef501d90bd8561f0d22bf65a
Diwakar Jha <diwakar.jha@w3villa.com>
2023-06-29 12:58:50 +0530

    crud with api with mongodb

--- index.js
+++ index.js
@@ -16,10 +16,26 @@ const config= require('./config')
 const products=require('./product')
 
 app.use(express.json())
+
 app.post('/',async (req,res)=>{
     let data=await new products(req.body);
     let result=await data.save();
     console.log(result)
     res.send(result)
+})
+app.get('/list',async (req,res)=>{
+    let data=await products.find();
+    console.log(data)
+    res.send(data);
+})
+app.delete('/delete/:_id', async (req,res)=>{
+    console.log(req.params)
+    let data=await products.deleteOne(req.params)
+    res.send(data)
+})
+app.put('/update/:_id',async (req,res)=>{
+    console.log(req.params)
+    let data= await products.updateOne({_id: req.params}, {$set: req.body})
+    res.send(data)
 })
 app.listen(5000);
