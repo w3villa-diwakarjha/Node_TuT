f192f1ea31dc87a3a7ebfa29f540b0b07333fa48
Diwakar Jha <diwakar.jha@w3villa.com>
2023-06-29 17:15:03 +0530

    Learn Os and event Emitter

--- index.js
+++ index.js
@@ -1,54 +1,35 @@
-// const express =require('express')
-// const config= require('./config')
-// const product= require('./product')
-// const app=express();
+// const os= require('os')
+// console.log(os.arch())
+// console.log(os.freemem()/(1024*1024*1024))
+// console.log(os.totalmem()/(1024*1024*1024))
+// console.log(os.hostname())
+// console.log(os.platform())
+// console.log(os.userInfo())
 
-// app.use(express.json())
+const express=require('express')
+const app=express();
+const EventEmitter= require('events');
 
-// app.get('/search/:key',async (req,res)=>{
-//     console.log(req.params)
-//     let data=await product.find(
-//         {
-//             "$or":[
-//                 {
-//                     "name": {$regex: req.params.key}
-//                 },
-//                 {
-//                     "brand": {$regex: req.params.key}
-//                 },
-//                 {
-//                     "category": {$regex: req.params.key}
-//                 }
-//             ]
-//         }
-//     );
-//     res.send(data);
-// })
-
-// app.listen(5000);
-
-const express = require('express')
-const app = express();
-const config = require('./config')
-const product = require('./product')
-const multer = require('multer')
-
-app.use(express.json())
-
-
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, 'uploads')
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.fieldname + "-" + Date.now() + ".jpg");
-        }
-    })
-}).single("brand")
-
-app.post('/upload', upload , (req, res) => {
-    console.log(req.body)
-    res.send("file Upload")
+let count=0;
+const event= new EventEmitter();
+event.on("countAPI",()=>{
+    count++;
+    console.log("event Called",count)
 })
+
+app.get('/',(req,res)=>{
+    res.send("api called");
+    event.emit("countAPI")
+})
+
+app.get('/search',(req,res)=>{
+    res.send("search api called")
+    event.emit("countAPI")
+})
+
+app.get('/update',(_,res)=>{
+    res.send("update api called")
+    event.emit("countAPI")
+})
+
 app.listen(5000);
